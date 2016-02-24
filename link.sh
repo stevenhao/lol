0eb4040ff276d46b83fddba7befabb3a765b4f47
stevenhao <stevenhao@gmail.com>
2016-02-23 19:54:07 -0500

    link.sh is verbose

--- link.sh
+++ link.sh
@@ -1,13 +1,13 @@
 cur=`pwd`
+(set -x
 for target in "$@"
 do
   if [ -f ~/.$target ]
     then
       today=`date +"%d-%m-%Y"`
-      echo mv -i ~/.$target old/.$target-$today
       mv -i ~/.$target old/.$target-$today
   fi
 
-  echo ln -s $cur/$target ~/.$target
   ln -s $cur/$target ~/.$target
 done
+)
